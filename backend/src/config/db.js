114f27a59e49779308126478d3d29dada6db336d
guilhxrme05 <gguihurtado@gmail.com>
2025-11-06 15:06:04 -0300

    teste deploy back

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,5 @@
 
-const { Pool } = require('pg');
+/*const { Pool } = require('pg');
 require('dotenv').config({ path: __dirname + '/../../.env' });
 
 
@@ -9,6 +9,32 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};*/
+
+const { Pool } = require('pg');
+
+// Esta linha vai ler a variável de ambiente correta
+const connectionString = process.env.DATABASE_URL;
+
+let sslConfig = undefined;
+
+// O Render define NODE_ENV=production automaticamente
+if (process.env.NODE_ENV === 'production') {
+  console.log("A executar em modo de produção (Render), a ativar SSL.");
+  sslConfig = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+}
+
+const pool = new Pool({
+  connectionString: connectionString,
+  ...sslConfig // Adiciona o SSL se estiver em produção
 });
 
 module.exports = {
